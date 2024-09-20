ec54b1058ecdb618029d3f6560f48319c096d185
Map987 <99967856+Map987@users.noreply.github.com>
2024-09-20 22:33:35 +0800

    Update run.py

--- run.py
+++ run.py
@@ -48,12 +48,13 @@ def default_serializer(obj):
     if isinstance(obj, PeerUser):
         return str(obj)  # 或者返回你希望存储的任何其他信息
     raise TypeError(f"Object of type {obj.__class__.__name__} is not JSON serializable")
-
- 
+STRING
+TELEGRAM_API_ID 
 import json
 from cryptography.fernet import Fernet
 import os
-
+CHANNEL_USERNAME
+TELEGRAM_API_HASH
 async def export_to_json(filename, data):
     encryption_key = os.getenv('TELEGRAM_API_HASH')
     # 如果没有提供密钥，则抛出异常
